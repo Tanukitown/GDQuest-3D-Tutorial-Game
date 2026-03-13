817d23a7be4b30712578cbc05423b742b55a8b6a
Tanukitown <kevin.mcguffin@gmail.com>
2026-03-12 21:49:15 -0400

    Spawn and kill mobs
    Up next: https://www.gdquest.com/library/first_3d_game_godot4_arena_fps/#scoring

--- mob/bat/mob.gd
+++ mob/bat/mob.gd
@@ -1,8 +1,37 @@
 extends RigidBody3D
 
+var health: float = 3
+var speed: float = randf_range(2.0, 4.0)
+
 
 @onready var bat_model: Node3D = $bat_model
+@onready var timer: Timer = %Timer
+@onready var player: CharacterBody3D = get_node("/root/Game/Player")
+
+
+func _physics_process(_delta: float) -> void:
+	var direction: Vector3 = global_position.direction_to(player.global_position)
+	direction.y = 0
+	linear_velocity = direction * speed
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 
 func take_damage():
+	if health == 0:
+		return
+
 	bat_model.hurt()
+	
+	health -= 1
+	if health == 0:
+		set_physics_process(false)
+		gravity_scale = 1.0
+		var direction: Vector3 = -1.0 * global_position.direction_to(player.global_position)
+		var random_upward_force: Vector3 = Vector3.UP * randf_range(1.0, 5.0)
+		apply_central_impulse(direction * 10.0 + random_upward_force)
+		timer.start()
+		lock_rotation = false
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
